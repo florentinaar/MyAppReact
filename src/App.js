6005b58d4def3c5c490b7c85cb336d1be4773d4d
fr48282 <78933950+fr48282@users.noreply.github.com>
2022-10-11 16:10:24 +0200

    added styledComponents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+const StyledDiv= styled.div`
+  padding:2%;
+  font-size:20px;
+  text-transform:capitalize;
+  background-color:lightgreen;
+`
+const StyledP= styled.div`
+
+  font-size:30px;
+  color:Black;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <StyledDiv>
+    <StyledP>
+    styled paraghrap
+    </StyledP>
+   </StyledDiv>
   );
 }
 
